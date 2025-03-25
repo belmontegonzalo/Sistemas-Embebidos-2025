1745199844abdf9ff9d4bb0aad8ef2620de52adb
gonzalobelmonte <gbelmonte@alumno.huergo.edu.ar>
2025-03-25 01:27:12 -0300

    Actualizo archivos

---
+++ Ejercicios_modulo_1/Ejercicio14/Ejercicio14.c
@@ -0,0 +1,50 @@
+// Dados 50 números enteros, informar el promedio de los mayores que 100 y la suma de los menores que –10.
+
+#include <stdio.h>
+int prom1 = 0, suma = 0, cant;
+int num[5];
+float promedio;
+
+void main(void) 
+{
+    printf("Hola! Tendras que ingresar 5 numeros enteros de tu preferencia, sin un rango determinado.\n");
+    for(int i=0; i < 5; i++) 
+    {
+        printf("\n Numero %d:",i+1);
+        scanf("%d", &num[i]);
+    }
+    
+    for (int i=0; i < 5; i++) 
+    {
+        if (num[i] > 100) 
+        {
+            prom1 = prom1 + num[i];
+            cant++;
+        }
+        
+        if (num[i] < -10) 
+        {
+            suma = suma + num[i];
+        }
+    }
+    
+    if (suma == 0) 
+    {
+        printf("\nNo hay valores menores a -10");
+    }
+    else
+    {
+        printf("\nLa suma de los numeros menores a -10 es %d", suma);
+    }
+        
+    if (prom1 == 0) 
+    {
+        printf("\nNo hay valores mayores a 100 ");
+    }
+    else 
+    {
+        promedio = (float)prom1 / cant;
+        printf("\nEl promedio de los numeros mayores a 100 es: %.2f", promedio);
+            
+    }
+}
---
+++ Ejercicios_modulo_1/Ejercicio15/Ejercicio15.c
@@ -0,0 +1,76 @@
+/* Se realiza una inspección en una fábrica de pinturas, y se detectaron 5 infracciones. De cada infracción se tomó nota de los siguientes datos:
+- Tipo de Infracción (1, 2, 3, ó 4)
+- Valor de la multa
+- Gravedad de la infracción (‘L’,‘M’, ‘G’)
+Se pide informar al final del proceso:
+ Los valores totales de la multa a pagar de acuerdo al tipo de gravedad.
+ La leyenda “Clausurar fábrica” si la cantidad de infracciones 3 y 4 con gravedad “G” sean mayor a 3.*/
+
+#include <stdio.h>
+#define INFRA_MAX 5
+
+struct Infraccion {
+    int tipo;
+    int valor;
+    char grav;
+};
+
+void main(void) 
+{
+    struct Infraccion infracciones[INFRA_MAX];
+    int cantI = 0, cantG = 0;
+    int valorL = 0, valorM = 0, valorG = 0;
+
+    printf("En este programa sabra, en base a las infracciones que tiene y su gravedad, que tan mal esta su fabrica.\n");
+    
+    printf("\nIngrese cuantas infracciones tiene en total (maximo %d):", INFRA_MAX);
+    scanf("%d", &cantI);
+    
+    getchar();
+    
+    for (int i = 0; i < cantI; i++)
+    {
+        printf("\nINFRACCION %d", i + 1);
+        printf("\nIngrese el tipo de infraccion (1, 2, 3 o 4): ");
+        scanf("%d", &infracciones[i].tipo);
+        
+        printf("\nIngrese el valor de la multa, sin signos: ");
+        scanf("%d", &infracciones[i].valor);
+        
+        getchar();
+        
+        printf("\nIngrese la gravedad de la infraccion (L, M, G): ");
+        scanf("%c", &infracciones[i].grav);
+        
+        if (infracciones[i].grav == 'L')
+        {
+            valorL = valorL + infracciones[i].valor;
+        }
+        else if (infracciones[i].grav == 'M')
+        {
+            valorM = valorM + infracciones[i].valor;
+        }
+        else if (infracciones[i].grav == 'G')
+        {
+            valorG = valorG + infracciones[i].valor;
+            
+            if (infracciones[i].tipo == 3 || infracciones[i].tipo == 4 && infracciones[i].grav == 'M')
+            {
+                cantG++;
+            }
+        }
+    }
+    
+    printf("\nEl valor de multas de gravedad L es: %d", valorL);
+    printf("\nEl valor de multas de gravedad M es: %d", valorM);
+    printf("\nEl valor de multas de gravedad G es: %d", valorG);
+    
+    if (cantG > 3)
+    {
+        printf("\nCLAUSURAR FABRICA.");
+    }
+    else
+    {
+        printf("\nNO HACE FALTA CLAUSURAR FABRICA.");
+    }
+}
